e2ef1290b3dfceca9fcb592683dddd3e1268ba1c
Code Subtle <korvanvitthal@gmail.com>
2025-11-07 09:30:17 +0530

    Project Completed!

--- 01-React-Fundamentals/04-Tailwind-UI-Project/src/App.jsx
+++ 01-React-Fundamentals/04-Tailwind-UI-Project/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Section1 from "./components/Section1/Section1";
+// import Section2 from "./components/Section2/Section2";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const users = [
+    {
+      img: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      intro: "",
+      color: "royalblue",
+      tag: "Satisfied",
+    },
+    {
+      img: "https://plus.unsplash.com/premium_photo-1661769159995-f3af0089875f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      color: "lightseagreen",
+      intro: "",
+      tag: "Underserved",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1498758536662-35b82cd15e29?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      color: "orange",
+      intro: "",
+      tag: "Underbanked",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1600275669439-14e40452d20b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      color: "pink",
+      intro: "",
+      tag: "Underwear",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1748785826435-83c5062a5737?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      intro: "",
+      color: "black",
+      tag: "Average",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Section1 users={users} />
+      {/* <Section2 /> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
