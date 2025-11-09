d5d95c448b32541179bf5713cde20614e082ee03
Code Subtle <korvanvitthal@gmail.com>
2025-11-09 11:20:27 +0530

    react-router-dom basic updated!

---
+++ 15-React-Router-DOM/basic-router-setup/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <h3>Vitthal Korvan</h3>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/product">Product</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
